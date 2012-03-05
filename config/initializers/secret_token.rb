08dafacf173178ead2b6683ab8edf1ca337e04bd
Your Name <toke@tzi.de>
2012-03-05 11:49:43 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AWEPLAN::Application.config.secret_token = '565e99bec51d6d73b36462eba8bd4dbfaf5986a5c4d7bf143094ad2e54f8c04d02e5602a30a4a83f4137e0c3244ff4f5bc6dd3aec1acba2f48af577d354fb805'
