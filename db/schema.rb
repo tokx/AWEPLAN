aa88278e2f555990520471a64748f43fc2195027
Your Name <toke@tzi.de>
2012-03-05 15:15:02 +0100

    fuuuck invitation deleted

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120305130122) do
-
-  create_table "groups", :force => true do |t|
-    t.string   "name"
-    t.string   "repo"
-    t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
+ActiveRecord::Schema.define(:version => 20120305125219) do
 
   create_table "users", :force => true do |t|
     t.string   "firstname"
